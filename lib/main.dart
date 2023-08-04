933475eb649a0353e010d40a7a182cefde5b213f
gabrielbiziach <gabrielbiziach@gmail.com>
2023-08-04 11:46:25 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Language Learning App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Language Learning App'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Language Learning App!'),
+      ),
+    );
+  }
+}
